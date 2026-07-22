466ef79453fd93e72bf8c6b63c8c353702816e8a
Rahul-agrawal-029 <100121588+Rahul-agrawal-029@users.noreply.github.com>
2026-07-22 18:35:30 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,29 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i=0; i< nums.length; i++){
+//             for(int j =i+1; j< nums.length; j++){
+//                 if(nums[i] + nums[j] == target){
+//                     return new int[]{i,j};
+//                 }
+//             }
+//         }
+//         return null;
+//     }
+// }
+
+class Solution{
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap();
+        for (int i=0; i< nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            else {
+                map.put((nums[i]),i);
+            }
+        }
+        System.out.println(map);
+        return null;
+    }
+
+}
